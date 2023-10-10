d383be61e541ed1415e43376a92449beccdad832
Pixel <calebrtomlinson@gmail.com>
2023-10-10 13:55:43 -0700

    Initial commit

---
+++ module/sheets/item-sheet.js
@@ -0,0 +1,8 @@
+
+
+export default class FSItemSheet extends ItemSheet {
+  get template() {
+    // return `systems/leovauntstarcraft/templates/sheets/${this.item.data.type}-sheet.html`;
+    return `systems/leovauntstarcraft/templates/sheets/item-sheet.html`;
+  }
+}
