f3ec4436bab24e6d79d452b020d59aaa128868ea
Pixel <calebrtomlinson@gmail.com>
2023-10-12 16:46:10 -0700

    Remove node bloat

--- src/init.ts
+++ src/init.ts
@@ -2,8 +2,10 @@
 //   console.log("Initializing leovauntstarcraft...");
 // });
 
+console.log("Loading Leovaunt Starcraft System, this is a debug message.");
+
 import "./module/config";
 
-Hooks.once("init", () => {
+Hooks.once("init", function() {
   console.log("Initializing Leovaunt Starcraft system...");
 });
