922638a7cf703270fc331094f7258cced7a64ee1
Pixel <calebrtomlinson@gmail.com>
2023-10-11 23:10:16 -0700

    WHY 3

--- init.js
+++
@@ -1,8 +0,0 @@
-// import FSItemSheet from "./module/sheets/item-sheet";
-
-Hooks.once("init", function() {
-  console.log("Initializing leovauntstarcraft...");
-
-  // Items.unregisterSheet("core", ItemSheet);
-  // Items.registerSheet("leovauntstarcraft", FSItemSheet, {makeDefault: true});
-});
