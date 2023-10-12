5b634490e6dc50650036767f818b3f2e03154c26
Pixel <calebrtomlinson@gmail.com>
2023-10-11 22:41:12 -0700

    Co-authored-by: supergirlanna@mochamail.com <supergirlanna@mochamail.com>

--- init.js
+++ init.js
@@ -1,7 +1,7 @@
 import FSItemSheet from "./module/sheets/item-sheet";
 
 Hooks.once("init", function() {
-  console.log("Initializing leovauntstarcraft");
+  console.log("Initializing leovauntstarcraft...");
 
   Items.unregisterSheet("core", ItemSheet);
   Items.registerSheet("leovauntstarcraft", FSItemSheet, {makeDefault: true});
