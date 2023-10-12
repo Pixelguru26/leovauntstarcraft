1214a72cec2f86ab85df4b28db6735b809adc4e7
Pixel <calebrtomlinson@gmail.com>
2023-10-11 22:23:47 -0700

    Boilerplate test

--- module/sheets/item-sheet.js
+++
@@ -1,8 +0,0 @@
-
-
-export default class FSItemSheet extends ItemSheet {
-  get template() {
-    // return `systems/leovauntstarcraft/templates/sheets/${this.item.data.type}-sheet.html`;
-    return `systems/leovauntstarcraft/templates/sheets/item-sheet.html`;
-  }
-}
